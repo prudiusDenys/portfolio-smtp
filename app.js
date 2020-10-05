deeaffadbb07e0bf7459c258a8c8a5e17135d749
prudiusDenys <prdslife@gmail.com>
2020-10-05 12:57:01 +0200

    env

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3010;
-
 const nodemailer = require("nodemailer");
 const cors = require('cors');
 const bodyParser = require('body-parser')
 
 
-let smtp_login = process.env.SMTP_LOGIN;
-let smtp_password = process.env.SMTP_PASSWORD;
+app.use(cors());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+// parse application/json
+app.use(bodyParser.json())
+
+
+let smtp_login = process.env.SMTP_LOGIN || '';
+let smtp_password = process.env.SMTP_PASSWORD || '';
 
 let transporter = nodemailer.createTransport({
   service: "gmail",
@@ -18,17 +25,6 @@ let transporter = nodemailer.createTransport({
   },
 });
 
-app.use(cors());
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
-// parse application/json
-app.use(bodyParser.json())
-
-app.get('/', function (req,res){
-  res.send('Hello World');
-})
-
 app.post('/sendMessage', async (req, res) => {
 
   let {message, phone, email, name} = req.body
@@ -37,7 +33,6 @@ app.post('/sendMessage', async (req, res) => {
     from: 'Portfolio form', // sender address
     to: "prdslife@gmail.com", // list of receivers
     subject: "Message from my portfolio", // Subject line
-    text: "", // plain text body
     html: `<b>Сообщение с вашего портфолио</b>
         <div>name: ${name}</div>
         <div>email: ${email}</div>
@@ -45,10 +40,8 @@ app.post('/sendMessage', async (req, res) => {
         <div>message: ${message}</div>`,
   });
 
-
-  res.send('Successful')
+  res.send('ok')
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
